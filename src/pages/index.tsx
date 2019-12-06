f06f0b6a61c714d57b00d4156e2a8aa5a2f2e40c
Benjamin LEVESQUE <14175665+benjlevesque@users.noreply.github.com>
2019-12-06 14:31:11 +0100

    format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
+import * as React from "react";
+import Link from "gatsby-link";
+import { graphql } from "gatsby";
+import Layout from "../layouts";
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
 // to generate all types from graphQL schema
@@ -7,30 +9,25 @@ interface IndexPageProps {
   data: {
     site: {
       siteMetadata: {
-        title: string
-      }
-    }
-  }
+        title: string;
+      };
+    };
+  };
 }
 
-export default class extends React.Component<IndexPageProps, {}> {
-  constructor(props: IndexPageProps, context: any) {
-    super(props, context)
-  }
-  public render() {
-    return (
-      <div>
-        <h1>Hi people</h1>
-        <p>
-          Welcome to your new{' '}
-          <strong>{this.props.data.site.siteMetadata.title}</strong> site.
-        </p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </div>
-    )
-  }
-}
+export default ({ data }: IndexPageProps) => (
+  <Layout>
+    <div>
+      <h1>Hi people</h1>
+      <p>
+        Welcome to your new <strong>{data.site.siteMetadata.title}</strong>{" "}
+        site.
+      </p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </div>
+  </Layout>
+);
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -40,4 +37,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
